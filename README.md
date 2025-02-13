5ff950a83f6639ed2b447d0ff3b95db94da1505d
Neil Cowie <thecowster@hotmail.co.uk>
2025-02-13 12:02:27 +0000

    first commit - uses single puppy dependency

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mymodule1
+
+go mod init github.com/ckdcowster/mymodule1
+
+go get github.com/ckdcowster/puppy@latest
+
+go mod tidy
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ckdcowster/puppy"
+)
+
+func main() {
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+}
